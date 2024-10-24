c03e05e61185ac92137b2169335e7e69474e8ea3
Nguyen Cong Cuong <c.ng.devie@gmail.com>
2024-10-24 17:22:59 +0700

    chore: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+    // Enable React to support React JSX components.
+    integrations: [react(), mdx()],
 });
